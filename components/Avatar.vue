4563055e53754cc1ad3863a4367c703a3038b4eb
BuJlJlu <golovashchenkom@ukr.net>
2025-02-19 13:36:37 +0200

    update avatar

--- components/Avatar.vue
+++ components/Avatar.vue
@@ -1,9 +1,18 @@
 <template>
-  <a href="/profile">
-    <img :src="data.avatar" alt="Avatar" class="w-12 h-12 rounded" />
-  </a>
+    <NuxtLink class="avatar" to="/profile">
+        <img class="avatar__img" :src="data.avatar" :alt="data.name"/>
+    </NuxtLink>
 </template>
 <script setup>
 const useAuthState = useAuth();
 const data = await useAuthState.fetchUser();
 </script>
+<style lang="scss" scoped>
+.avatar {
+    &__img {
+        width: 40px;
+        height: 40px;
+        border-radius: 100px;
+    }
+}
+</style>
