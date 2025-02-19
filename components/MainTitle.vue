2770a7a97f7c0c1b37e1e3a91638ea4c149f95c2
BuJlJlu <golovashchenkom@ukr.net>
2025-02-19 13:38:04 +0200

    add main title

---
+++ components/MainTitle.vue
@@ -0,0 +1,62 @@
+<template>
+    <header class="title">
+        <p class="value">
+           <span class="value__label">
+                {{ label }}
+           </span> 
+        </p>
+        <MainButton 
+            v-if="btnLabel"
+            @click="$emit('btnClick')"
+            :label="btnLabel"
+            tag="button"
+            class="title__btn"
+            :theme="themeType.secondary"
+        />
+    </header>
+</template>
+<script>
+import { themeType } from '~~/constants/theme.constants';
+
+export default {
+    name: "MainTitle",
+    props: {
+        label: {
+            type: String,
+            required: true
+        },
+        btnLabel: {
+            type: String,
+            default: '',
+        }
+    },
+
+    setup() {
+        return {
+            themeType
+        }
+    }
+};
+</script>
+<style lang="scss" scoped>
+.title {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    &__btn {
+        margin-left: 16px;
+    }
+}
+.value {
+    padding-top: 9px;
+    padding-bottom: 9px;
+    &__label {
+        display: block;
+        line-height: 1.2;
+        font-family: $sf_mono;
+        font-size: 17px;
+        font-weight: 600;
+        color: #000;
+    }
+}
+</style>
