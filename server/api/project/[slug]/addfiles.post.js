1a2e63fc75d1a24713a0f4d8babd55e3c9154349
vovastelmashchuk <vovochkastelmashchuk@gmail.com>
2025-03-04 22:07:27 +0200

    add upload file to exist project

---
+++ server/api/project/[slug]/addfiles.post.js
@@ -0,0 +1,30 @@
+import { defineEventHandler, createError } from "h3";
+
+import { connectDB } from "~~/server/db/mongo";
+import { saveFilesToProject } from "~~/server/core/project/dxf";
+
+export default defineEventHandler(async (event) => {
+  const userId = event.context?.auth?.userId;
+  if (!userId) {
+    throw createError({ statusCode: 401, message: "Unauthorized" });
+  }
+  const projectSlug = getRouterParam(event, "slug");
+
+  const db = await connectDB();
+
+  const project = await db.collection("projects").findOne({
+    slug: projectSlug,
+  });
+
+  const projectOwnerId = project.ownerId;
+
+  if (projectOwnerId !== userId) {
+    throw createError({ statusCode: 403, message: "Forbidden" });
+  }
+
+  saveFilesToProject(event, projectSlug);
+
+  return {
+    slug: projectSlug,
+  };
+});
