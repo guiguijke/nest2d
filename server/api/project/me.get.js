e2b4a05e56c72f65a11b6065652e1522f314c0a5
vovastelmashchuk <vovochkastelmashchuk@gmail.com>
2025-03-04 23:02:26 +0200

    add create at and nest request count

--- server/api/project/me.get.js
+++ server/api/project/me.get.js
@@ -9,20 +9,39 @@ export default defineEventHandler(async (event) => {
     .collection("projects")
     .find({ ownerId: userId })
     .sort({ uploadedAt: -1 })
-    .project({ slug: 1, name: 1 })
+    .project({ slug: 1, name: 1, createdAt: 1 })
     .toArray();
 
-  const uiProjects = userProjects.map(mapProject);
+  const projectSlugs = userProjects.map((project) => project.slug);
+
+  const nestRequestCounts = await db
+    .collection("nest_request")
+    .aggregate([
+      { $match: { projectSlug: { $in: projectSlugs } } }, // Filter only relevant projectSlugs
+      { $group: { _id: "$projectSlug", count: { $sum: 1 } } }, // Count occurrences of each projectSlug
+    ])
+    .toArray();
+
+  const countMap = nestRequestCounts.reduce((acc, item) => {
+    acc[item._id] = item.count;
+    return acc;
+  }, {});
+
+  const uiProjects = userProjects.map((project) =>
+    mapProject(project, countMap)
+  );
 
   return {
     projects: uiProjects,
   };
 });
 
-function mapProject(project) {
+function mapProject(project, countMap) {
   return {
     slug: project.slug,
     name: project.name,
     imageUrl: "/placeholder.svg",
+    createdAt: project.createdAt,
+    nestRequestCount: countMap[project.slug] || 0, // Ensure 0 if no matches
   };
 }
