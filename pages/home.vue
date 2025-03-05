09175b8807be4402029b55d2c79408c12ce9d0a6
BuJlJlu <golovashchenkom@ukr.net>
2025-03-05 16:06:25 +0200

    refactor: replace DragAndDrop component

--- pages/home.vue
+++ pages/home.vue
@@ -4,7 +4,17 @@
             label="Upload your .DXF files"
             class="home__title"
         />
-        <DragAndDrop /> 
+        <DxfUpload @files="handleSubmit" />
+        <p class="home__text">
+            All files will be save secure and available only for you
+        </p>
+
+        <div
+            v-if="error"
+            class="home__error"
+        >
+            {{ error }}
+        </div>
     </div>
 </template>
 
@@ -13,12 +23,57 @@ definePageMeta({
     layout: "auth",
     middleware: "auth",
 });
+
+const router = useRouter();
+
+const { actions } = globalStore;
+const { setProjects } = actions;
+
+const error = ref('')
+
+const handleSubmit = async (files) => {
+    error.value = "";
+
+    if (files.length === 0) {
+        error.value = "At least one DXF file is required.";
+        return;
+    }
+
+    const formData = new FormData();
+    files.forEach((file) => formData.append("dxf", file));
+
+    const response = await fetch("/api/project", {
+        method: "POST",
+        body: formData,
+    });
+
+    if (!response.ok) {
+        const errorData = await response.json();
+        error.value = errorData.message;
+    } else {
+        const data = await response.json();
+        await setProjects()
+        await router.push({ path: `/project/${data.slug}` });
+    }
+}
 </script>
 
 <style lang="scss" scoped>
 .home {
+    text-align: center;
     &__title {
         margin-bottom: 16px;
+    }
+    &__text {
+        margin-top: 16px;
+        color: var(--label-tertiary);
+    }
+    &__error {
+        margin-top: 16px;
+        padding: 12px;
+        background-color: var(--error-background);
+        border: solid 1px var(--error-border);
+        border-radius: 8px;
     }
 }
 </style>
