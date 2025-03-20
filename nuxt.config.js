6812cfbd68683aaafb7e257f329998744f6a2a1e
BuJlJlu <golovashchenkom@ukr.net>
2025-03-20 11:28:47 +0200

    refactor: update Nuxt configuration for improved SEO and site metadata, add new favicon assets

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+import { schemaWebSite } from './utils/schema'
+
 export default defineNuxtConfig({
     compatibilityDate: "2024-11-01",
     devtools: { enabled: true },
@@ -13,7 +15,7 @@ export default defineNuxtConfig({
         '@/assets/css/main.css',
     ],
     plugins: [
-        '~/plugins/theme.js'
+        '@/plugins/theme.js'
     ],
     vite: {
         css: {
@@ -27,6 +29,90 @@ export default defineNuxtConfig({
                     `
                 }
             }
+        }
+    },
+
+    site: {
+        url: 'https://nest2d.stelmashchuk.dev/',
+        name: 'Nest2D'
+    },
+
+    build: {
+        treeShake: true,
+        extractCSS: true,
+        optimization: {
+            splitChunks: {
+                layouts: true,
+                pages: true,
+                commons: true
+            }
+        }
+    },
+
+    app: {
+        head: {
+            htmlAttrs: {
+                lang: 'en'
+            },
+            title: 'Nest2D - Smart Nesting for Laser Cutting',
+            meta: [
+                { charset: 'utf-8' },
+                {
+                    name: 'viewport',
+                    content: 'width=device-width, initial-scale=1'
+                },
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'Upload your DXF files, input material dimensions, and let Nest2d maximize your material usage. Fully open-source and built for efficiency.'
+                },
+                {
+                    hid: 'keywords',
+                    name: 'keywords',
+                    content: 'DXF files, material dimensions, Nest2D, maximize material usage, open-source, efficiency, smart nesting, laser cutting'
+                },
+                {
+                    hid: 'robots',
+                    name: 'robots',
+                    content: 'index, follow'
+                },
+                {
+                    hid: 'author',
+                    name: 'author',
+                    content: 'Nest2D'
+                }
+            ],
+            link: [
+                {
+                    rel: 'apple-touch-icon',
+                    sizes: '180x180',
+                    href: '/favicon/apple-touch-icon.png'
+                },
+                {
+                    rel: 'icon',
+                    type: 'image/png',
+                    sizes: '32x32',
+                    href: '/favicon/favicon-32x32.png'
+                },
+                {
+                    rel: 'icon',
+                    type: 'image/png',
+                    sizes: '16x16',
+                    href: '/favicon/favicon-16x16.png'
+                },
+                {
+                    rel: 'icon',
+                    type: 'image/x-icon',
+                    href: '/favicon/favicon.ico'
+                }
+            ],
+            script: [
+                {
+                    async: true,
+                    type: 'application/ld+json',
+                    children: JSON.stringify(schemaWebSite)
+                }
+            ]
         }
     },
 });
