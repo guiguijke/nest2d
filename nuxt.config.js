28995255a3bdb87dd3c501effca8136867f42ac6
BuJlJlu <golovashchenkom@ukr.net>
2025-03-20 11:53:00 +0200

    refactor: uncomment nitro configuration in Nuxt settings for asset caching

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,39 +51,20 @@ export default defineNuxtConfig({
         }
     },
 
-    // nitro: {
-    //     compressPublicAssets: true,
-    //     routeRules: {
-    //         '/icons/**': {
-    //             headers: {
-    //                 'cache-control': `public,max-age=${year},s-maxage=${year}`
-    //             }
-    //         },
-    //         '/fonts/**': {
-    //             headers: {
-    //                 'cache-control': `public,max-age=${year},s-maxage=${year}`
-    //             }
-    //         }
-    //     },
-    //     storage: {
-    //         cache: {
-    //             driver: 'fs',
-    //             options: {
-    //                 base: './.nitro/cache'
-    //             }
-    //         }
-    //     },
-    //     compress: {
-    //         brotli: {
-    //             enabled: true,
-    //             zlevel: 11
-    //         },
-    //         gzip: {
-    //             enabled: true,
-    //             level: 9
-    //         }
-    //     }
-    // },
+    nitro: {
+        routeRules: {
+            '/icons/**': {
+                headers: {
+                    'cache-control': `public,max-age=${year},s-maxage=${year}`
+                }
+            },
+            '/fonts/**': {
+                headers: {
+                    'cache-control': `public,max-age=${year},s-maxage=${year}`
+                }
+            }
+        },
+    },
 
     app: {
         head: {
