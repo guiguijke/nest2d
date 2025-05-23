2ca7c2680c60c965a4186cb16d0ecd6f503bd848
BuJlJlu <golovashchenkom@ukr.net>
2025-05-23 22:47:42 +0300

    Add structured content for header, features, screenshots, how it works, and FAQ sections

---
+++ data/index.js
@@ -0,0 +1,96 @@
+export const header = {
+    title: 'Nesting For plotters, laser & plasma cutters, and other CNC machines',
+    text: 'Upload your DXF files, input material dimensions, and let Nest2d reduce material waste.'
+}
+export const features = {
+    title: 'Features',
+    list: [
+        {
+            title: 'DXF File Support',
+            text: 'Easily upload DXF files for precision laser cutting.'
+        },
+        {
+            title: 'Save money',
+            text: 'Minimize waste by optimizing part arrangement.'
+        },
+        {
+            title: 'No client resource require',
+            text: 'All calculation done by server.'
+        },
+        {
+            title: 'Open Source',
+            text: 'Fully open-source and built for efficiency.'
+        }
+    ]
+}
+export const screenshots = {
+    title: 'Intuitive design',
+    list: [
+        {
+            src: '/screenshots/project-ui.png',
+        },
+        {
+            src: '/screenshots/nest-result.png',
+        },
+        {
+            src: '/screenshots/preview.png',
+        }
+    ]
+}
+export const howItWorks = {
+    title: 'How It Works',
+    list: [
+        {
+            title: 'Upload DXF Files',
+            text: 'Drag and drop or browse your files to upload.'
+        },
+        {
+            title: 'Enter Material Size',
+            text: 'Specify the dimensions of your material sheet.'
+        },
+        {
+            title: 'Optimize and Download',
+            text: 'Get an optimized layout ready for cutting.'
+        }
+    ]
+}
+export const started = {
+    title: 'Get Started Today',
+    text: 'Sign up and start optimizing your laser cutting projects in minutes.'
+}
+export const faq = {
+    title: 'Frequently Asked Questions',
+    text: 'Stuck on something? We’re here to help with all your questions and answers in one place.',
+    list: [
+        {
+            title: 'Is Nest2d has a free trial?',
+            firstPart: 'Yes, you can try us for free until your read a limit of free credding. If you want, login and send a message to me via support chat.'
+        },
+        {
+            title: 'What is your cancellation policy?',
+            firstPart: 'I understand that things change. You can cancel your payment any time, i will provide a full refound.'
+        },
+        {
+            title: 'Is it a one-time payment?',
+            firstPart: 'Just a one-time payment! No recurring charges or surprises, we promise. We’re just as sick of recurring charges as you are.'
+        },
+        {
+            title: 'What file formats do you support?',
+            firstPart: 'Currently, we support DXF files for laser cutting.'
+        },
+        {
+            title: 'Can I contribute to the project?',
+            firstPart: 'Absolutely! Check out our',
+            link: 'GitHub repository',
+            linkHref: 'https://github.com/VovaStelmashchuk/nest2D',
+            target: '_blank',
+            secondPart: 'to contribute.'
+        },
+        {
+            title: 'I still have questions.',
+            firstPart: 'Fill free to contact me via support chat or contant me via email',
+            link: 'vovochkastelmashchuk@gmail.com',
+            linkHref: 'mailto:vovochkastelmashchuk@gmail.com',
+        },
+    ]
+}
--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,10 @@
     <div class="main">
         <section class="main__header header">
             <h2 class="title title--large">
-                Nesting For plotters, laser & plasma cutters, and other CNC
-                machines
+                {{ header.title }}
             </h2>
             <p class="header__text">
-                Upload your DXF files, input material dimensions, and let Nest2d
-                reduce material waste.
+                {{ header.text }}
             </p>
             <MainButton
                 :theme="themeType.primary"
@@ -18,62 +16,34 @@
         </section>
         <section class="main__features features" id="features">
             <h3 class="features__title title title--medium">
-                Features
+                {{ features.title }}
             </h3>
             <div class="features__list features-list">
-                <div class="features-list__card">
+                <div 
+                    v-for="featuresItem in features.list" 
+                    :key="featuresItem.title"
+                    class="features-list__card"
+                >
                     <h4 class="features-list__title title">
-                        DXF File Support
+                        {{ featuresItem.title }}
                     </h4>
                     <p class="features-list__text">
-                        Easily upload DXF files for precision laser cutting.
-                    </p>
-                </div>
-                <div class="features-list__card">
-                    <h4 class="features-list__title title">
-                        Save money
-                    </h4>
-                    <p class="features-list__text">
-                        Minimize waste by optimizing part arrangement.
-                    </p>
-                </div>
-                <div class="features-list__card">
-                    <h4 class="features-list__title title">
-                        No client resource require
-                    </h4>
-                    <p class="features-list__text">
-                        All calculation done by server.
-                    </p>
-                </div>
-                <div class="features-list__card">
-                    <h4 class="features-list__title title">
-                        Open Source
-                    </h4>
-                    <p class="features-list__text">
-                        Fully open-source and built for efficiency.
+                        {{ featuresItem.text }}
                     </p>
                 </div>
             </div>
         </section>
         <section class="main__screenshots screenshots" id="screenshots">
             <h3 class="screenshots__title title title--medium">
-                Intuitive design
+                {{ screenshots.title }}
             </h3>
             <div
                 class="screenshots__list screenshots-list"
             >
                 <img
-                    src="/screenshots/project-ui.png"
-                    alt="project ui"
-                    class="screenshots-list__img"
-                />
-                <img
-                    src="/screenshots/nest-result.png"
-                    alt="project ui"
-                    class="screenshots-list__img"
-                />
-                <img
-                    src="/screenshots/preview.png"
+                    v-for="screenshot in screenshots.list" 
+                    :key="screenshot.src"
+                    :src="screenshot.src"
                     alt="project ui"
                     class="screenshots-list__img"
                 />
@@ -81,51 +51,32 @@
         </section>
         <section class="main__works works" id="how-it-works">
             <h3 class="works__title title title--medium">
-                How It Works
+                {{ howItWorks.title }}
             </h3>
             <div class="works__list works-list">
-                <div class="works-list__item">
+                <div 
+                    v-for="(howItWorksItem, howItWorksIdex) in howItWorks.list" 
+                    :key="howItWorksItem.title"
+                    class="works-list__item"
+                >
                     <span class="works-list__number">
-                        1
+                        {{ ++howItWorksIdex }}
                     </span>
                     <h4 class="works-list__title title">
-                        Upload DXF Files
+                        {{ howItWorksItem.title }}
                     </h4>
                     <p class="works-list__text">
-                        Drag and drop or browse your files to upload.
-                    </p>
-                </div>
-                <div class="works-list__item">
-                    <span class="works-list__number">
-                        2
-                    </span>
-                    <h4 class="works-list__title title">
-                        Enter Material Size
-                    </h4>
-                    <p class="works-list__text">
-                        Specify the dimensions of your material sheet.
-                    </p>
-                </div>
-                <div class="works-list__item">
-                    <span class="works-list__number">
-                        3
-                    </span>
-                    <h4 class="works-list__title title">
-                        Optimize and Download
-                    </h4>
-                    <p class="works-list__text">
-                        Get an optimized layout ready for cutting.
+                        {{ howItWorksItem.text }}
                     </p>
                 </div>
             </div>
         </section>
         <section class="main__started started" id="get-started">
             <h3 class="started__title title title--medium">
-                Get Started Today
+                {{ started.title }}
             </h3>
             <p class="started__text">
-                Sign up and start optimizing your laser cutting projects in
-                minutes.
+                {{ started.text }}
             </p>
             <MainButton
                 :theme="themeType.primary"
@@ -136,78 +87,33 @@
         </section>
         <section class="main__faq faq" id="faq">
             <h3 class="faq__title title title--medium">
-                Frequently Asked Questions
+                {{ faq.title }}
             </h3>
             <p class="faq__text">
-                Stuck on something? We’re here to help with all your questions
-                and answers in one place.
+                {{ faq.text }}
             </p>
             <div class="faq__list faq-list">
-                <div class="faq-list__item">
+                <div 
+                    v-for="faqItem in faq.list" 
+                    :key="faq.title"
+                    class="faq-list__item"
+                >
                     <h4 class="faq-list__title title">
-                        Is Nest2d has a free trial?
+                        {{ faqItem.title }}
                     </h4>
                     <p>
-                        Yes, you can try us for free until your read a limit of free
-                        credding. If you want, login and send a message to me via
-                        support chat.
-                    </p>
-                </div>
-                <div class="faq-list__item">
-                    <h4 class="faq-list__title title">
-                        What is your cancellation policy?
-                    </h4>
-                    <p>
-                        I understand that things change. You can cancel your payment
-                        any time, i will provide a full refound.
-                    </p>
-                </div>
-                <div class="faq-list__item">
-                    <h4 class="faq-list__title title">
-                        Is it a one-time payment?
-                    </h4>
-                    <p>
-                        Just a one-time payment! No recurring charges or surprises,
-                        we promise. We’re just as sick of recurring charges as you
-                        are.
-                    </p>
-                </div>
-                <div class="faq-list__item">
-                    <h4 class="faq-list__title title">
-                        What file formats do you support?
-                    </h4>
-                    <p>
-                        Currently, we support DXF files for laser cutting.
-                    </p>
-                </div>
-                <div class="faq-list__item">
-                    <h4 class="faq-list__title title">
-                        Can I contribute to the project?
-                    </h4>
-                    <p>
-                        Absolutely! Check out our
-                        <a
+                        {{ faqItem.firstPart }}
+                        <a  
+                            v-if="faqItem.link"
                             class="faq-list__link"
-                            href="https://github.com/VovaStelmashchuk/nest2D"
-                            target="_blank"
+                            :href="faqItem.linkHref"
+                            :target="faqItem.target ? faqItem.target : '_self'"
                         >
-                            GitHub repository</a>
-                        to contribute.
-                    </p>
-                </div>
-                <div class="faq-list__item">
-                    <h4 class="faq-list__title title">
-                        I still have questions.
-                    </h4>
-                    <p>
-                        Fill free to contact me via support chat or contant me via
-                        email
-                        <a 
-                            class="faq-list__link"
-                            href="mailto:vovochkastelmashchuk@gmail.com"
-                            target="_blank"
-                        >
-                            vovochkastelmashchuk@gmail.com</a>
+                            {{ faqItem.link }}
+                        </a>
+                        <template v-if="faqItem.secondPart">
+                            {{ ' ' + faqItem.secondPart }}
+                        </template>
                     </p>
                 </div>
             </div>
@@ -218,7 +124,7 @@
 definePageMeta({
     middleware: 'auth'
 })
-
+import { header, features, screenshots, howItWorks, started, faq } from '~~/data/index'
 import { themeType } from '~~/constants/theme.constants'
 const loginDialog = useLoginDialog()
 </script>
@@ -306,6 +212,7 @@ const loginDialog = useLoginDialog()
         flex-basis: calc(100% / 3);
     }
     &__number {
+        color: var(--accent-primary);
         width: 64px;
         height: 64px;
         border-radius: 50%;
