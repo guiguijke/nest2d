a3dfcf063e5463b8e265f26afcf25be4d348f6e1
vovastelmashchuk <vovochkastelmashchuk@gmail.com>
2025-07-27 13:08:07 +0300

    small update

--- workers/fileprocessing/main.py
+++ workers/fileprocessing/main.py
@@ -1,3 +1,5 @@
+import time
+import traceback
 from utils.logger import setup_logger
 from utils.mongo import db, _client
 
@@ -11,3 +13,27 @@ try:
 except Exception as e:
     logger.error(f"Database connection failed: {e}")
     raise e
+
+user_dxf_files = db["user_dxf_files"]
+
+while True:
+    logger.info("Worker file processing try to find new files to process")
+    doc = user_dxf_files.find_one_and_update(
+        {"processingStatus": "pending"},
+        {"$set": {"processingStatus": "processing"}},
+        return_document=ReturnDocument.AFTER
+    )
+    
+    if doc is None:
+        time.sleep(5)
+        continue
+    
+    try:
+        doJobProject(doc)
+    except Exception as e:
+        logger.error("Error in project processing", extra={"error": str(e), "traceback": traceback.format_exc()})
+        user_dxf_files.update_one(
+            {"_id": doc["_id"]},
+            {"$set": {"processingStatus": "error",
+                      "processingError": str(e)}}
+        )
