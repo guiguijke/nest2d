417e287fdddc3d6b84abbccbcfbde8d75b90eb22
vovastelmashchuk <vovochkastelmashchuk@gmail.com>
2025-07-27 15:22:16 +0300

    add mongo uri to envvar

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,10 +8,6 @@ if [ -f /run/secrets/gmail_app_password ]; then
   export NUXT_GMAIL_APP_PASSWORD="$(cat /run/secrets/gmail_app_password)"
 fi
 
-if [ -f /run/secrets/mongo_uri ]; then
-  export NUXT_MONGO_URI="$(cat /run/secrets/mongo_uri)"
-fi
-
 if [ -f /run/secrets/stripe_secret_key ]; then
   export NUXT_STRIPE_SECRET_KEY="$(cat /run/secrets/stripe_secret_key)"
 fi
