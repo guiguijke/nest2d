dd0a7cba77c62b4c289db198f2fecf006bf25dcc
vovastelmashchuk <vovochkastelmashchuk@gmail.com>
2025-08-10 11:36:12 +0300

    new engine

--- workers/fileprocessing/core/geometry/build_geometry.py
+++ workers/fileprocessing/core/geometry/build_geometry.py
@@ -8,6 +8,7 @@ from dxf_parser import DxfEntityGeometry, convert_entity_to_shapely
 from ezdxf.document import Drawing
 from shapely.geometry import Polygon
 from shapely.validation import make_valid
+from shapely.ops import unary_union
 
 @dataclass(slots=True)
 class ClosedPolygon:
@@ -19,37 +20,27 @@ def merge_dxf_entities_into_polygons(dxf_entities: Iterable[DxfEntityGeometry], 
     for dxf_entity in dxf_entities:
         area = dxf_entity.geometry.area
         if area > 1e-6:
-            result.append(ClosedPolygon(geometry=make_valid(dxf_entity.geometry.exterior.buffer(tolerance)), handles=[dxf_entity.handle]))
+            result.append(ClosedPolygon(geometry=make_valid(dxf_entity.geometry.convex_hull.buffer(tolerance)), handles=[dxf_entity.handle]))
         
     while True:
         print(f"Merging {len(result)} polygons")
         result.sort(key=lambda cp: cp.geometry.area, reverse=True)
-        n = len(result)
-        isFound = False
-        for i in range(n):
-            for j in range(i + 1, n):
+        old_size = len(result)
+        for i in range(len(result)):
+            to_remove = []
+            isFound = False
+            for j in range(i + 1, len(result)):
                 if result[i].geometry.intersects(result[j].geometry):
                     result[i].geometry = result[i].geometry.union(result[j].geometry).convex_hull
                     result[i].handles.extend(result[j].handles)
-                    result.pop(j)
+                    to_remove.append(j)
                     isFound = True
-                    break
-                if result[i].geometry.contains(result[j].geometry):
-                    result[i].geometry = result[i].geometry.convex_hull
-                    result[i].handles.extend(result[j].handles)
-                    result.pop(j)
-                    isFound = True
-                    break
-                if result[j].geometry.contains(result[i].geometry):
-                    result[j].geometry = result[j].geometry.convex_hull
-                    result[j].handles.extend(result[i].handles)
-                    result.pop(i)
-                    isFound = True
-                    break
             if isFound:
+                for j in sorted(to_remove, reverse=True):
+                    result.pop(j)
                 break
                 
-        if n == len(result):
+        if old_size == len(result):
             break
     
     return result
