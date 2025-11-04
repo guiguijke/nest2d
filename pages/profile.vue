3f99bea43d7d15901a2ecf0b7cca6c0f9952d82e
Guillaume <guiguijke@gmail.com>
2025-11-05 00:01:04 +0100

    modified:   pages/profile.vue

--- pages/profile.vue
+++ pages/profile.vue
@@ -6,9 +6,8 @@
             <MainButton :theme="themeType.primary" @click="logoutHandler" label="Logout" class="profile__btn" />
         </div>
         <UserBalance class="profile__balance" />
-        <BuyCredits />
-        <div class="payhip-buttons" style="margin-top: 20px;">
-            <h3>Choose a Credit Pack</h3>
+        <div class="payhip-buttons">
+            <h3>Choisis un Pack de Crédits</h3>
             <button class="pack-button starter" @click="buyStarter">Starter Credit Pack - 1€</button>
             <button class="pack-button workshop" @click="buyWorkshop">Workshop Credit Pack - 5€</button>
             <button class="pack-button power" @click="buyPower">Power Credit Pack - 19.99€ + 25% bonus</button>
@@ -34,19 +33,20 @@ const logoutHandler = async () => {
     await logout();
     router.push({ path: '/' })
 };
-methods: {
-    // Add these methods
-    buyStarter() {
-      window.location.href = 'https://shop.aplasma.fr/b/S3mNb';
-    },
-    buyWorkshop() {
-      window.location.href = 'https://shop.aplasma.fr/b/8amDK';
-    },
-    buyPower() {
-      window.location.href = 'https://shop.aplasma.fr/b/hWzEf';
-    }
-  }
+
+const buyStarter = () => {
+    window.location.href = 'https://shop.aplasma.fr/b/S3mNb';
+};
+
+const buyWorkshop = () => {
+    window.location.href = 'https://shop.aplasma.fr/b/8amDK';
+};
+
+const buyPower = () => {
+    window.location.href = 'https://shop.aplasma.fr/b/hWzEf';
+};
 </script>
+
 <style lang="scss" scoped>
 .profile {
     &__title {
@@ -67,20 +67,25 @@ methods: {
         margin-bottom: 24px;
     }
 }
+
 .payhip-buttons {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
+    margin-top: 20px;
 }
+
 .pack-button {
-  padding: 10px 20px;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  font-size: 16px;
-  width: 80%;  /* Adjust for layout */
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: 16px;
+    width: 80%;  /* Ajuste pour s'adapter à la largeur */
+    text-align: center;
 }
+
 .starter { background-color: #4caf50; color: white; }
 .workshop { background-color: #2196f3; color: white; }
 .power { background-color: #f44336; color: white; }
